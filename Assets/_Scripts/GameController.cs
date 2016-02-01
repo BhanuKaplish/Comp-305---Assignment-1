f85857d02fd3aca480ebab8fe987682765b0e12c
BhanuKaplish <bhanu.kaplish@gmail.com>
2016-02-01 00:18:48 -0500

    Made GameController and added Asteroids

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    //Public Indtance Variables
+    public int asteroidNumber = 3;
+    public AsteroidController asteroid;
+
+	// Use this for initialization
+	void Start () {
+        initialize();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Private method
+    private void initialize()
+    {
+        for(int asteroidCount = 0; asteroidCount < this.asteroidNumber; asteroidCount++)
+        {
+            Instantiate(asteroid.gameObject);
+        }
+    }
+}
